6b5809268b4f1fe51192ca0fb74da2322f5d36cd
Vicente Debrun <debrunvicente@gmail.com>
2023-03-26 19:35:19 -0700

    state way back

--- models/state.py
+++ models/state.py
@@ -1,21 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from os import getenv
+
 
 class State(BaseModel):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", backref="state", cascade="all, delete")
-    else:
-        @property
-        def cities(self):
-            """Returns a list of cities that belong to the current State"""
-            from models import storage
-            from models.city import City
-            cities = storage.all(City)
-            return [city for city in cities.values() if city.state_id == self.id]
+    name = ""
