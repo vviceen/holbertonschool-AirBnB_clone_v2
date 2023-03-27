71d5fcfa180908fbc5b2a0b33d7fa2077ccf7c27
Vicente Debrun <debrunvicente@gmail.com>
2023-03-26 19:36:31 -0700

    init back and db_storage dont run well

--- models/__init__.py
+++ models/__init__.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
-from models.engine.db_storage import DBStorage
+"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-from os import getenv
-
-class Base():
-    pass
-
-from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.state import State
-from models.review import Review
 
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    storage = DBStorage()
-else:
-    storage = FileStorage()
+storage = FileStorage()
 storage.reload()
